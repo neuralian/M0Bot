86512060a2a2e7f80a34cc29211c42064f1e81bc
Mike Paulin <neuralian@gmail.com>
2019-10-10 09:04:10 +1300

    M0Bot initial code
    Framework for mode selection and mode execution.  '*' on remote to enter mode selection mode, then another button to select behaviour mode (intended 0-9, but any button except '*' could be used.   Code for each mode can be inserted in Switch statement as e.g.'d by 'remote control' model.  Note code is written for the black remotes from Banggood.com, which use NEC  encoding but  with different key assignments.  See IRCODES.h for button-code assignments.

---
+++ M0Bot01/M0Bot01.ino
@@ -0,0 +1,110 @@
+// M0Bot01
+// Trinket M0 Bristlebot
+// MGP October 2019
+
+// Load hardware definitions & infrared codes
+#include "M0BOT.h"
+
+// Infrared library
+#include <IRremote.h>
+IRrecv irrecv(IRRCV_PIN);
+decode_results results;
+
+unsigned MODE = -99;
+unsigned Message = 0;
+unsigned LastMessage = 0;
+unsigned Speed = 0;
+bool Left_Turn = false;
+bool Right_Turn = false;
+
+void setup() {
+
+  Serial.begin(115200); // open serial port
+
+  irrecv.enableIRIn(); // Start the IR receiver
+  
+  pinMode(LMOTOR, OUTPUT);
+  pinMode(RMOTOR, OUTPUT);
+
+}
+
+void loop() {
+
+
+
+  // get IR message
+  if (irrecv.decode(&results)) { // if new message received
+    Message = results.value;
+    if (Message==IRCODE_REPEAT) Message = LastMessage;
+    else LastMessage = Message;
+   // Serial.println(Message);
+    
+    if (MODE==MODE_SELECT) {
+      switch (Message) {
+        case IRCODE_0:
+          MODE = MODE_REMOTE;
+          Serial.println("Remote control mode");
+          break;
+        case IRCODE_1:
+          MODE = MODE_MEANDER;
+          Serial.println("Meander mode");
+          break;          
+        case IRCODE_2:
+          MODE = MODE_BROWNIAN;
+          Serial.println("Brownian motion mode");
+          break;                    
+      }
+    }
+    else {
+      if (Message == IRCODE_STAR){
+           MODE = MODE_SELECT;
+           Serial.println("Mode select mode");
+      }
+      else switch (MODE) {
+
+       case MODE_REMOTE:
+          Left_Turn = false;
+          Right_Turn = false;
+          switch (Message){
+            case IRCODE_UP:
+              Speed += ACCELERATION;
+              if (Speed>255) Speed = 255;
+              break;
+            case IRCODE_DOWN:
+              // nb Speed is unsigned, can't go <0
+              if (Speed>=ACCELERATION) Speed -= ACCELERATION;
+              else Speed = 0;
+              break;              
+            case IRCODE_LEFT:
+              Left_Turn = true;
+              break;                
+            case IRCODE_RIGHT:
+              Right_Turn = true;
+              break;               
+        }
+      }
+//    Serial.print("Speed = " );  Serial.print(Speed); 
+//    Serial.print(", Left_Turn = "); Serial.print(Left_Turn);
+//    Serial.print(", Right_Turn = "); Serial.println(Right_Turn);
+    
+    }
+    irrecv.resume(); // Receive the next value
+
+  }
+  
+  // move
+  if (Left_Turn) {
+    digitalWrite(LMOTOR, LOW);
+    Serial.println("L");
+  }
+  else if (random(100) < Speed) digitalWrite(LMOTOR, HIGH);
+       else digitalWrite(LMOTOR, LOW);
+  if (Right_Turn) digitalWrite(RMOTOR, LOW);
+  else if (random(100) < Speed) digitalWrite(RMOTOR, HIGH);
+       else digitalWrite(RMOTOR, LOW);
+  
+  delay(1);
+  
+
+  
+}
---
+++ library/M0BOT.h
@@ -0,0 +1,14 @@
+// Trinket M0 Bristlebot header
+
+#include "M0BOT_IRCODES.h"
+
+#define IRRCV_PIN 0
+#define RMOTOR 4
+#define LMOTOR 1
+#define ACCELERATION 10
+
+// Behaviour modes
+#define MODE_SELECT  0
+#define MODE_REMOTE  1
+#define MODE_MEANDER 2
+#define MODE_BROWNIAN 3
